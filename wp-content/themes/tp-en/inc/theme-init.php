50dcf6ac061c00ca124e314f4a44955368e33f85
trung nguyen thanh <trungnguyenthanh70@gmail.com>
2023-10-03 09:12:57 +0700

    UPdate version php

--- wp-content/themes/tp-en/inc/theme-init.php
+++ wp-content/themes/tp-en/inc/theme-init.php
@@ -434,3 +434,11 @@ class feature_post extends WP_Widget
 		return $instance;
 	}
 }
+// remove wp version number from scripts and styles
+function remove_css_js_version( $src ) {
+	if( strpos( $src, '?ver=' ) )
+			$src = remove_query_arg( 'ver', $src );
+	return $src;
+}
+add_filter( 'style_loader_src', 'remove_css_js_version', 9999 );
+add_filter( 'script_loader_src', 'remove_css_js_version', 9999 );
